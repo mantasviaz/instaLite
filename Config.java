cd04e33e33be0174bfbd85aaa5813a2ede69fea7
Mantas Viazmitinas <158117948+mantasviaz@users.noreply.github.com>
2024-04-24 20:00:57 -0400

    Create Config.java

---
+++ Config.java
@@ -0,0 +1,12 @@
+public class Config {
+    public static final String CHROMA_CLIENT_PORT = "8000";
+    public static final String CHROMA_DB_FOLDER_NAME = "chroma_db";
+    public static final String CHROMA_DB_PATH = "chroma_db/";
+    public static final String AWS_REGION = "us-east-1";
+    //public static final String CSV_FILE_PATH = "input/actor_embeddings.csv";
+    public static final String S3_BUCKET_NAME = "images-upenn-nets2120-2024sp-leftovers";
+    //public static final String DYNAMO_TABLE_NAME = "imdb-data";
+    //public static final String CHROMA_DB_NAME = "imdb-photos";
+    //public static final String CHROMA_BUCKET_NAME = "imdb-chroma-upenn-nets2120-2024sp-mantasviaz";
+    //public static final String SPARK_APP_NAME = "ImageIndexingApp";
+}
