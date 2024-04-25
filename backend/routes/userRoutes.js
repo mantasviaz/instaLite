e4b04ed06e8336f98957b3081918424eafc396e9
johntmajernik <johnmaj@seas.upenn.edu>
2024-04-25 00:37:11 +0000

    tables and route defs for user/post

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+<<<<<<< HEAD
 const userController = require('../controller/userController');
 import { Router } from 'express';
 const router = Router();
@@ -9,6 +10,12 @@ export default router;
 
 
 router.post('/signup', userController.registerUser);
+=======
+const router = express.Router();
+const userController = require('../controller/userController');
+
+router.post('/register', userController.registerUser);
+>>>>>>> 3174762 (tables and route defs for user/post)
 router.post('/login', userController.loginUser);
 router.patch('/:userId', userController.updateUserProfile);
 router.delete('/:userId', userController.deleteUser);
