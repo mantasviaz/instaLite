d7d6a90f1ca6770bd0b88eff235b519590c20579
Kevin Lee <kleekev@sas.upenn.edu>
2024-04-30 04:10:45 +0000

    added and udpated styling for flex-start

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
     console.log(image_post_test);
   }, []);
   return (
-    <div className="flex max-h-full flex-1 flex-col items-center justify-start overflow-y-auto">
+    <div className="flex-start max-h-full flex-1 flex-col overflow-y-auto">
       <h1 className=" bg-red-300 text-3xl font-bold underline">Home Page</h1>
       {image_post_test.map((post, idx) => (
         <ImagePost
