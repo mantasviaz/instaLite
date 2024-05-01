c2df50dc34fe36b87e32212ff54d9b9a8f9916e8
Kevin Lee <kleekev@sas.upenn.edu>
2024-05-01 04:52:56 +0000

    Added image post page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,6 @@ import Login from "./pages/Login";
 import Profile from "./pages/Profile";
 import FriendList from "./pages/FriendList";
 import ImagePostPage from "./pages/ImagePostPage";
-import TextPostPage from "./pages/TextPostPage";
 
 function App() {
   return (
@@ -18,11 +17,6 @@ function App() {
           <Route path="/home" element={<Home />} />
           <Route path="/friends" element={<FriendList />} />
           <Route path="/image-post" element={<ImagePostPage />} />
-          <Route path="/text-post" element={<TextPostPage />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/profile/:userId" element={<Profile />} />
         </Routes>
       </BrowserRouter>
     </div>
