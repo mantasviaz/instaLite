8776ef900c8c800172baae3a149f8cc89aca4c41
Kevin Lee <kleekev@sas.upenn.edu>
2024-05-01 04:02:16 +0000

    Added text post test

---
+++ frontend/src/test/text-post.js
@@ -0,0 +1,53 @@
+const text_posts = [
+  {
+    username: "user1",
+    text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit quidem minima saepe obcaecati quos excepturi totam delectus minus consequatur quibusdam vitae, deleniti voluptatum ab nobis vel qui natus alias. Maiores?',
+    num_of_likes: 15
+  },
+  {
+    username: "user2",
+    text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit quidem minima saepe obcaecati quos excepturi totam delectus minus consequatur quibusdam vitae, deleniti voluptatum ab nobis vel qui natus alias. Maiores?',
+    num_of_likes: 20
+  },
+  {
+    username: "user3",
+    text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit quidem minima saepe obcaecati quos excepturi totam delectus minus consequatur quibusdam vitae, deleniti voluptatum ab nobis vel qui natus alias. Maiores?',
+    num_of_likes: 10
+  },
+  {
+    username: "user4",
+    text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit quidem minima saepe obcaecati quos excepturi totam delectus minus consequatur quibusdam vitae, deleniti voluptatum ab nobis vel qui natus alias. Maiores?',
+    num_of_likes: 5
+  },
+  {
+    username: "user5",
+    text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit quidem minima saepe obcaecati quos excepturi totam delectus minus consequatur quibusdam vitae, deleniti voluptatum ab nobis vel qui natus alias. Maiores?',
+    num_of_likes: 8
+  },
+  {
+    username: "user6",
+    text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit quidem minima saepe obcaecati quos excepturi totam delectus minus consequatur quibusdam vitae, deleniti voluptatum ab nobis vel qui natus alias. Maiores?',
+    num_of_likes: 12
+  },
+  {
+    username: "user7",
+    text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit quidem minima saepe obcaecati quos excepturi totam delectus minus consequatur quibusdam vitae, deleniti voluptatum ab nobis vel qui natus alias. Maiores?',
+    num_of_likes: 25
+  },
+  {
+    username: "user8",
+    text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit quidem minima saepe obcaecati quos excepturi totam delectus minus consequatur quibusdam vitae, deleniti voluptatum ab nobis vel qui natus alias. Maiores?',
+    num_of_likes: 18
+  },
+  {
+    username: "user9",
+    text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit quidem minima saepe obcaecati quos excepturi totam delectus minus consequatur quibusdam vitae, deleniti voluptatum ab nobis vel qui natus alias. Maiores?',
+    num_of_likes: 30
+  },
+  {
+    username: "user10",
+    text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit quidem minima saepe obcaecati quos excepturi totam delectus minus consequatur quibusdam vitae, deleniti voluptatum ab nobis vel qui natus alias. Maiores?',
+    num_of_likes: 7
+  }
+];
+export default text_posts;
