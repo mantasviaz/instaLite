2c0a07b5c56feeb71f9dd34ea97902ec8cb54886
Kevin Lee <kleekev@sas.upenn.edu>
2024-05-01 04:02:07 +0000

    Created text posts

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,12 @@
 import React, { useEffect } from "react";
+
+// Test posts
 import image_post_test from "../test/image-post";
+import text_posts from "../test/text-post";
+
 import ImagePost from "../components/ImagePost";
 import CreatePost from "../components/CreatePost";
+import TextPost from "../components/TextPost";
 
 function Home() {
   useEffect(() => {
@@ -16,6 +21,14 @@ function Home() {
           text={post.text}
           img_link={post.img}
           created_date={post.created_at}
+          key={idx}
+        />
+      ))}
+      {text_posts.map((post, idx) => (
+        <TextPost
+          username={post.username}
+          content={post.text}
+          num_of_likes={post.num_of_likes}
           key={idx}
         />
       ))}
