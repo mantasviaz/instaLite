3fa389efa3450797a56b037e738fa47d3cdbf7a5
Kevin Lee <kleekev@sas.upenn.edu>
2024-05-01 04:01:29 +0000

    Commented out some work

--- frontend/src/components/CreatePost.jsx
+++ frontend/src/components/CreatePost.jsx
@@ -45,10 +45,11 @@ function CreatePost() {
 
   const handlePost = (event) => {
     event.preventDefault();
-    const hashtags = content
-      .replace(/\s/g, "")
-      .split("#")
-      .slice(1, content.length);
+    // The backend can deal with parsing the hashtags
+    // const hashtags = content
+    //   .replace(/\s/g, "")
+    //   .split("#")
+    //   .slice(1, content.length);
 
     console.log(hashtags);
     console.log("SUBMIT POST");
