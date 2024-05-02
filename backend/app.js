e372c12826a647d32c7733c4393aad36c1ab4a94
johntmajernik <johnmaj@seas.upenn.edu>
2024-05-02 18:39:55 +0000

    Merge 'backend' folder from feature-branch into main

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const sequelize = require('./config/dbConfig'); 
 const userRoutes = require('./routes/userRoutes');
+const postRoutes = require('./routes/postRoutes'); // Import post routes
 const User = require('./models/user'); 
+const Post = require('./models/post'); 
 
 const app = express();
 
 app.use(express.json());
 app.use('/api/users', userRoutes);
+app.use('/api/posts', postRoutes); // Mount post routes
 
 // Sync all models
 sequelize.sync().then(() => {
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const { createPost, getPosts, deletePost } = require('../controllers/postController');
+const { createPost, getPosts, deletePost } = require('../controller/postController');
 const router = express.Router();
 
 router.post('/', createPost);
-router.get('/', getPosts);
+router.get('/user/:userId', getPosts);
 router.delete('/:postId', deletePost);
 
 module.exports = router;
