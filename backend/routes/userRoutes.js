552c1362bdabe2da1a9780f737681b1f146a3978
Irene Kim <kirene@sas.upenn.edu>
2024-05-05 18:56:58 -0400

    minor changes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controller/userController');
 
-router.post('/register', userController.registerUser);
+router.post('/signup', userController.registerUser);
 router.post('/login', userController.loginUser);
 router.patch('/:userId', userController.updateUserProfile);
 router.delete('/:userId', userController.deleteUser);
