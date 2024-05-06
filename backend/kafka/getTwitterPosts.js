7af7b2d7176402ef933608e587c08d773c248381
Kevin Lee <kleekev@sas.upenn.edu>
2024-05-06 17:30:00 +0000

    Twitter User created

--- backend/kafka/getTwitterPosts.js
+++ backend/kafka/getTwitterPosts.js
@@ -29,13 +29,6 @@ const createUser = async (user_id) => {
   if (user) {
     return user;
   }
-  user = createRandomUser();
-  try {
-    const newUser = await User.create({ ...user, userId: user_id });
-    return newUser;
-  } catch (error) {
-    return;
-  }
 };
 
 // Create a post
@@ -89,7 +82,7 @@ const isValidJson = (string) => {
 // Upload tweets from kafka to database
 const getTwitterPosts = async () => {
   // Consuming
-  await consumer.connect();
+  // await consumer.connect();
   console.log(`Following topic ${config['consumer-topic']}`);
   // Subscribe to topic
   await consumer.subscribe({ topic: config['consumer-topic'], fromBeginning: true, partitions: [0] }); // Will set fromBeginning to false in actual feed
@@ -99,26 +92,36 @@ const getTwitterPosts = async () => {
       let twitterPost;
       if (isValidJson(message.value.toString())) {
         twitterPost = JSON.parse(message.value.toString());
-        const twitterPost = JSON.parse(message.value.toString());
-        const [user, hashtags] = await Promise.all([createUser(twitterPost.author_id), createHashtag(twitterPost.hashtags)]);
-        const newPost = await createPost(user.userId, twitterPost.text, new Date(twitterPost.created_at));
-        await createPostHashtag(newPost, hashtags);
+        // const [user, hashtags] = await Promise.all([createUser(4), createHashtag(twitterPost.hashtags)]);
+        // const newPost = await createPost(user.userId, twitterPost.text, new Date(twitterPost.created_at));
+        // await createPostHashtag(newPost, hashtags);
 
         console.log({
           twitterPost: twitterPost,
-          user: user,
-          hashtags: hashtags,
-          newPost: newPost,
+          // user: user,
+          // hashtags: hashtags,
+          // newPost: newPost,
         });
       }
       // Commits an offset so that it resume to the previous offset before shutting down
       // await consumer.commitOffsets([{ topic, partition, offset: message.offset }]);
     },
   });
-  await consumer.seek({ topic: config['consumer-topic'], partition: 0, offset: '0' });
-  // await consumer.disconnect();
+  // await consumer.seek({ topic: config['consumer-topic'], partition: 0, offset: '0' });
+};
+const runConsumer = async () => {
+  try {
+    await consumer.connect();
+    await getTwitterPosts();
+    await consumer.disconnect();
+  } catch (error) {
+    console.error('Error:', error);
+  }
 };
 
-getTwitterPosts().catch(console.error);
+runConsumer().catch(console.error);
+
+// Schedule the consumer to run every hour
+setInterval(runConsumer, (3600000 / 60) * 0.5);
 
 // module.exports = getTwitterPosts;
