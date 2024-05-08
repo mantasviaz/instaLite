7a3f251135978ca588d29dbdc39168abe3e4bef2
Kevin Lee <kleekev@sas.upenn.edu>
2024-05-08 01:49:16 +0000

    updated user to include status

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -4,123 +4,130 @@ const sequelize = require('../config/dbConfig.js');
 
 // Register a new user
 exports.registerUser = async (req, res) => {
-    try {
-        if (!req.body.username || !req.body.email || !req.body.password || !req.body.firstName || !req.body.lastName) {
-            return res.status(500).send({ error: 'Validation Error', message: 'Required fields are missing' });
-        }
-        const hashedPassword = await bcrypt.hash(req.body.password, 10);
-        const user = await User.create({
-            username: req.body.username,
-            email: req.body.email,
-            password_hash: hashedPassword,
-            first_name: req.body.firstName,
-            last_name: req.body.lastName
-        });
-        const result = user.toJSON();
-        delete result.password_hash;
-        res.status(201).send(result);
-    } catch (error) {
-        console.error("Error registering user:", error);
-        res.status(500).send({ error: 'Internal Server Error', message: error.message });
+  try {
+    if (!req.body.username || !req.body.email || !req.body.password || !req.body.firstName || !req.body.lastName) {
+      return res.status(500).send({ error: 'Validation Error', message: 'Required fields are missing' });
     }
-  };
-  
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const user = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password_hash: hashedPassword,
+      first_name: req.body.firstName,
+      last_name: req.body.lastName,
+    });
+    const result = user.toJSON();
+    delete result.password_hash;
+    res.status(201).send(result);
+  } catch (error) {
+    console.error('Error registering user:', error);
+    res.status(500).send({ error: 'Internal Server Error', message: error.message });
+  }
+};
 
 // Login user
 exports.loginUser = async (req, res) => {
-    try {
-        const { email, password } = req.body;
-        const user = await User.findOne({ where: { email } });
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ where: { email } });
 
-        if (!user) {
-            console.log("User not found for email:", email);
-            return res.status(401).send("Authentication failed: User not found");
-        }
-
-        console.log("Found user:", user.email);
-        console.log("Stored hash:", user.password_hash);
-        const passwordValid = await bcrypt.compare(password, user.password_hash);
-
-        console.log("Submitted password:", password);
-        console.log("Is password valid:", passwordValid);
-
-        if (passwordValid) {
-            const result = user.toJSON();
-            delete result.password_hash;  // Remove sensitive data before sending response
-            res.status(200).send(result);
-        } else {
-            res.status(401).send("Authentication failed: Incorrect password");
-        }
-    } catch (error) {
-        console.error("Error logging in user:", error);
-        res.status(500).send({ error: 'Internal Server Error', message: error.message });
+    if (!user) {
+      console.log('User not found for email:', email);
+      return res.status(401).send('Authentication failed: User not found');
     }
+
+    console.log('Found user:', user.email);
+    console.log('Stored hash:', user.password_hash);
+    const passwordValid = await bcrypt.compare(password, user.password_hash);
+
+    console.log('Submitted password:', password);
+    console.log('Is password valid:', passwordValid);
+
+    if (passwordValid) {
+      const result = user.toJSON();
+      delete result.password_hash; // Remove sensitive data before sending response
+      res.status(200).send(result);
+    } else {
+      res.status(401).send('Authentication failed: Incorrect password');
+    }
+  } catch (error) {
+    console.error('Error logging in user:', error);
+    res.status(500).send({ error: 'Internal Server Error', message: error.message });
+  }
 };
-
-
 
 //update
 exports.updateUserProfile = async (req, res) => {
-    let transaction;
+  let transaction;
 
-    try {
-        //console.log("Received userId for update:", req.params.userId);
-        transaction = await sequelize.transaction();
+  try {
+    //console.log("Received userId for update:", req.params.userId);
+    transaction = await sequelize.transaction();
 
-        const userExists = await User.findByPk(req.params.userId, { transaction });
-        //console.log("User exists:", !!userExists);
-        if (!userExists) {
-            await transaction.rollback();
-            return res.status(404).send("User not found");
-        }
-
-        //console.log("Update data:", req.body);
-        // Ensure the request body has the correct properties
-        const updateData = {
-            first_name: req.body.first_name,
-            last_name: req.body.last_name,
-            updated_at: new Date()  // Update the 'updated_at' field to current time
-        };
-
-        const [updated] = await User.update(updateData, {
-            where: { userId: req.params.userId },
-            transaction: transaction
-        });
-        
-        if (updated) {
-            const updatedUser = await User.findByPk(req.params.userId, { transaction });
-            await transaction.commit();
-            res.status(200).send(updatedUser);
-        } else {
-            await transaction.rollback();
-            res.status(404).send("User not found");
-        }
-    } catch (error) {
-        if (transaction) await transaction.rollback();
-        console.error("Error updating user:", error);
-        res.status(500).send({ error: 'Internal Server Error', message: error.message });
+    const userExists = await User.findByPk(req.params.userId, { transaction });
+    //console.log("User exists:", !!userExists);
+    if (!userExists) {
+      await transaction.rollback();
+      return res.status(404).send('User not found');
     }
+
+    //console.log("Update data:", req.body);
+    // Ensure the request body has the correct properties
+    const updateData = {
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      updated_at: new Date(), // Update the 'updated_at' field to current time
+    };
+
+    const [updated] = await User.update(updateData, {
+      where: { userId: req.params.userId },
+      transaction: transaction,
+    });
+
+    if (updated) {
+      const updatedUser = await User.findByPk(req.params.userId, { transaction });
+      await transaction.commit();
+      res.status(200).send(updatedUser);
+    } else {
+      await transaction.rollback();
+      res.status(404).send('User not found');
+    }
+  } catch (error) {
+    if (transaction) await transaction.rollback();
+    console.error('Error updating user:', error);
+    res.status(500).send({ error: 'Internal Server Error', message: error.message });
+  }
 };
-
-
-
-
-
-
-
 
 // Delete a user
 exports.deleteUser = async (req, res) => {
-    try {
-        const deleted = await User.destroy({
-            where: { userId: req.params.userId }
-        });
-        if (deleted) {
-            res.status(200).send("User deleted successfully");
-        } else {
-            res.status(404).send("User not found");
-        }
-    } catch (error) {
-        res.status(500).send({ error: 'Internal Server Error', message: error.message });
+  try {
+    const deleted = await User.destroy({
+      where: { userId: req.params.userId },
+    });
+    if (deleted) {
+      res.status(200).send('User deleted successfully');
+    } else {
+      res.status(404).send('User not found');
     }
+  } catch (error) {
+    res.status(500).send({ error: 'Internal Server Error', message: error.message });
+  }
+};
+
+exports.updateUserStatus = async (req, res) => {
+  try {
+    const { userId, status } = req.body;
+    console.log('HERE');
+    const user = await User.findOne({
+      where: {
+        userId: userId,
+      },
+    });
+    user.status = status;
+    await user.save();
+    res.status(200).send(user);
+  } catch (error) {
+    res.status(500).send({ error: 'Failed to change user status', message: error.message });
+  }
 };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,5 +6,6 @@ router.post('/register', userController.registerUser);
 router.post('/login', userController.loginUser);
 router.patch('/:userId', userController.updateUserProfile);
 router.delete('/:userId', userController.deleteUser);
+router.post('/status', userController.updateUserStatus);
 
 module.exports = router;
