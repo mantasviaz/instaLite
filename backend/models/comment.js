db3c97343878cee334a39edef79e95be2ad448ca
Kevin Lee <kleekev@sas.upenn.edu>
2024-05-10 01:31:17 +0000

    updated comment api

--- backend/models/comment.js
+++ backend/models/comment.js
@@ -1,20 +1,26 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/dbConfig');
+const User = require('./user');
 
 class Comment extends Model {}
 
-Comment.init({
-  commentId: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-  postId: { type: DataTypes.INTEGER, allowNull: false },
-  userId: { type: DataTypes.INTEGER, allowNull: false },
-  text: { type: DataTypes.STRING },
-  created_at: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
-  updated_at: { type: DataTypes.DATE, defaultValue: DataTypes.NOW }
-}, {
-  sequelize,
-  modelName: 'Comment',
-  timestamps: false,
-  tableName: 'comments'
-});
+Comment.init(
+  {
+    commentId: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    postId: { type: DataTypes.INTEGER, allowNull: false },
+    userId: { type: DataTypes.INTEGER, allowNull: false },
+    text: { type: DataTypes.STRING },
+    created_at: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
+    updated_at: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
+  },
+  {
+    sequelize,
+    modelName: 'Comment',
+    timestamps: false,
+    tableName: 'comments',
+  }
+);
+
+Comment.belongsTo(User, { foreignKey: 'userId' });
 
 module.exports = Comment;
